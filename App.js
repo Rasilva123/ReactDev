c07eabeb984ffa0a21649f70c018bf4bf5383b3d
Rahyan Ramos <rahyanramos.dev@gmail.com>
2025-05-01 22:10:02 -0400

    Refactor App component to integrate theme provider and add missing screens

--- App.js
+++ App.js
@@ -1,31 +1,37 @@
 import React from 'react';
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
+import { ThemeProvider as ContextThemeProvider, useTheme } from './theme/ThemeContext';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
+import LessonsScreen from './screens/LessonsScreen';
+import LessonDetailScreen from './screens/LessonDetailScreen';
 import QuizScreen from './screens/QuizScreen';
 import AboutScreen from './screens/AboutScreen';
-import { ThemeProvider, useTheme } from './theme/ThemeContext';
+import { ThemeProvider as StyledProvider } from 'styled-components/native';
 
 const Stack = createStackNavigator();
 
 function AppContent() {
-  const { darkMode } = useTheme(); // pegamos o estado do tema
-
+  const { darkMode, theme } = useTheme();
   return (
-    <NavigationContainer theme={darkMode ? DarkTheme : DefaultTheme}>
-      <Stack.Navigator>
-        <Stack.Screen name="Início" component={HomeScreen} />
-        <Stack.Screen name="Quiz" component={QuizScreen} />
-        <Stack.Screen name="Sobre o React Native" component={AboutScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <StyledProvider theme={theme.colors}>
+      <NavigationContainer theme={darkMode ? DarkTheme : DefaultTheme}>
+        <Stack.Navigator>
+            <Stack.Screen name="Início" component={HomeScreen} />
+            <Stack.Screen name="Quiz" component={QuizScreen} />
+            <Stack.Screen name="Sobre o React Native" component={AboutScreen} />
+            <Stack.Screen name="Lições" component={LessonsScreen} />
+            <Stack.Screen name="Detalhes da Lição" component={LessonDetailScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </StyledProvider>
   );
 }
 
 export default function App() {
   return (
-    <ThemeProvider>
+    <ContextThemeProvider>
       <AppContent />
-    </ThemeProvider>
+    </ContextThemeProvider>
   );
 }
